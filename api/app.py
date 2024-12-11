b4f8658e80c64e4ad23396b54ec0fec381ced9ec
Oscar Mur <oscarsusmur@gmail.com>
2024-12-11 18:13:52 +0100

    rescale images

--- api/app.py
+++ api/app.py
@@ -1,26 +1,26 @@
-from flask import Flask, send_from_directory
-from config import Config
-from models import db
-from routes import main
-from flask_jwt_extended import JWTManager
+from flask import Config, Flask, send_from_directory, jsonify
+from models import db, Image
 import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 
-# Set up Flask-JWT-Extended 
-app.config['JWT_SECRET_KEY'] = 'supersecretpassword'
-jwt = JWTManager(app)
+IMAGE_DIRECTORY = '/home/2DAM/images'
 
-app.register_blueprint(main)
-
-@app.route('/images/<filename>')
-def serve_image(filename):
-    image_directory = '/home/2DAM/images' 
-    return send_from_directory(image_directory, filename)
+@app.route('/image/<int:image_id>', methods=['GET'])
+def get_image(image_id):
+    image = Image.query.get(image_id)
+    
+    if image:
+        image_path = os.path.join(IMAGE_DIRECTORY, image.filename)
+        
+        if os.path.exists(image_path):
+            return send_from_directory(IMAGE_DIRECTORY, image.filename)
+        else:
+            return jsonify({"error": "Image file not found"}), 404
+    else:
+        return jsonify({"error": "Image not found"}), 404
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
