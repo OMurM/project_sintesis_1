077c0b5c83fc6cd54a55f9d5c53d22a76666b85c
Oscar Mur <oscarsusmur@gmail.com>
2024-12-17 19:19:02 +0100

    updated images routes

--- api/routes/images.py
+++ api/routes/images.py
@@ -14,3 +14,21 @@ def get_images_on_offer():
         images.append(image.serialize())
     
     return jsonify(images), 200
+
+@main.route('/images/all_products', methods=['GET'])
+def get_all_products():
+    # Join Product and Image tables without filtering by offer
+    all_products = db.session.query(Product, Image).join(Image, Product.image_id == Image.image_id).all()
+
+    # Serialize the products and their associated images
+    products_with_images = []
+    for product, image in all_products:
+        product_data = product.serialize()
+        image_data = image.serialize()
+        
+        product_data['image'] = image_data
+        products_with_images.append(product_data)
+
+    return jsonify(products_with_images), 200
+
+
