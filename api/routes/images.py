812dcada0f9583eebe881ce15eb22147c84fc5a9
Oscar Mur <oscarsusmur@gmail.com>
2024-12-18 19:55:05 +0100

    updated Images

--- api/routes/images.py
+++ api/routes/images.py
@@ -1,20 +1,26 @@
+import logging
 from flask import request, jsonify
 from . import main
 from models import db, Image, Product
 
-# Endpoint to featch the images
+# Set up logging configuration
+logging.basicConfig(level=logging.DEBUG)
+
+# Endpoint to fetch the images on offer
 @main.route('/images/on_offer', methods=['GET'])
 def get_images_on_offer():
-    # Join Porduct and Image tables
+    # Join Product and Image tables
     products_on_offer = db.session.query(Product, Image).join(Image, Product.image_id == Image.image_id).filter(Product.offer == True).all()
 
     # Serialize the images
     images = []
     for product, image in products_on_offer:
-        images.append(image.serialize())
-    
+        image_data = image.serialize()
+        images.append(image_data)
+        
     return jsonify(images), 200
 
+# Endpoint to fetch all products and their images
 @main.route('/images/all_products', methods=['GET'])
 def get_all_products():
     # Join Product and Image tables without filtering by offer
@@ -30,5 +36,3 @@ def get_all_products():
         products_with_images.append(product_data)
 
     return jsonify(products_with_images), 200
-
-
